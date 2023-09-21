598fa0b03d07f22cdaa9b47a32a34383444f71c4
Diluka Theekshana <dilukatheekshana.github@gmail.com>
2023-09-21 23:01:31 +0530

    Blood Bank Management System

---
+++ Blood Bank Management System/src/DbUtils.java
@@ -0,0 +1,20 @@
+
+import java.sql.ResultSet;
+import javax.swing.table.TableModel;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+class DbUtils {
+
+    static TableModel ResultSetToTableModel(ResultSet rs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
